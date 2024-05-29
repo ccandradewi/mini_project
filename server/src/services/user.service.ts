ffe1267265ec1a40d6e4191dc54ec353c1f73496
astnab <astnab@gmail.com>
2024-05-29 17:01:58 +0700

    fixed registration (reference), add errors, add templates

--- server/src/services/user.service.ts
+++ server/src/services/user.service.ts
@@ -8,6 +8,9 @@ import ReferralCode from "../libs/referral";
 import { transporter } from "../libs/nodemailer";
 import { SECRET_KEY } from "../config/config";
 import { verify } from "jsonwebtoken";
+import fs, { readFile } from "fs";
+import { join } from "path";
+import { render } from "mustache";
 
 class UserService {
   async userLogin(req: Request) {
@@ -47,8 +50,8 @@ class UserService {
 
     delete data.password;
 
-    const accessToken = createToken(data, "1hr");
-    const refreshToken = createToken({ id: data.id }, "24hr");
+    const accessToken = createToken(data, "15m");
+    const refreshToken = createToken({ id: data.id }, "1hr");
     return { accessToken, refreshToken, role: data.role };
   }
 
@@ -70,7 +73,8 @@ class UserService {
         OR: [{ email }, { username }],
       },
     });
-    if (existingUser.length) throw new Error("username/email already used");
+    if (existingUser.length)
+      throw new Error("Username/email has already been used");
 
     const hashPass = await hashPassword(password);
 
@@ -87,7 +91,6 @@ class UserService {
       first_name,
       last_name,
       phone_number,
-      reference_code,
       referral_code: referralCode ? referralCode[0] : null,
     };
 
@@ -99,62 +102,66 @@ class UserService {
     if (reference_code) {
       const referrer = await prisma.user.findFirst({
         where: {
-          referral_code: data.reference_code,
+          referral_code: reference_code,
         },
       });
       console.log("referrer found", referrer);
 
-      // add new user using referrer's ref code
-      newUser = await prisma.user.create({
-        data,
-      });
       console.log("new user created with reference", newUser);
 
-      if (referrer) {
-        try {
-          // check if referrer already exists in voucher point (their referral is used)
-          const referrerPoint = await prisma.voucherPoint.findFirst({
-            where: {
-              user_id: referrer.id,
-            },
-          });
+      if (!referrer) throw new Error("ref code not found");
 
-          // add 10000 to last point amount
-          const newPoint = (referrerPoint?.point || 0) + 10000;
-          const expirationDate = new Date();
-          expirationDate.setMonth(expirationDate.getMonth() + 3);
+      // check if referrer already exists in voucher point (their referral is used)
+      const referrerPoint = await prisma.voucherPoint.findFirst({
+        where: {
+          user_id: referrer.id,
+        },
+      });
 
-          if (referrerPoint) {
-            // update referrer's point in voucherpoint
-            await prisma.voucherPoint.update({
-              where: { id: referrerPoint.id },
-              data: {
-                point: newPoint,
-                expired_date: expirationDate,
-                voucher: referrerPoint.voucher,
-                isValid: false,
-              },
-            });
-          } else {
-            // if referral code's never been used, it will create new data in voucherpoint
-            await prisma.voucherPoint.create({
-              data: {
-                user_id: referrer.id,
-                point: newPoint,
-                expired_date: expirationDate,
-                voucher: 0,
-                isValid: false,
-              },
-            });
-          }
-          console.log("point added for referrer");
-        } catch (error) {
-          console.log("error adding point for referrer");
-        }
+      // add 10000 to last point amount
+      const newPoint =
+        referrerPoint?.expired_date! > new Date()
+          ? (referrerPoint?.point || 0) + 10000
+          : 10000;
+          
+      const expirationDate = new Date();
+      expirationDate.setMonth(expirationDate.getMonth() + 3);
+
+      if (referrerPoint) {
+        // update referrer's point in voucherpoint
+        await prisma.voucherPoint.update({
+          where: { id: referrerPoint.id },
+          data: {
+            point: newPoint,
+            expired_date: expirationDate,
+            voucher: referrerPoint.voucher,
+            isValid: false,
+          },
+        });
+      } else {
+        // if referral code's never been used, it will create new data in voucherpoint
+        await prisma.voucherPoint.create({
+          data: {
+            user_id: referrer.id,
+            point: newPoint,
+            expired_date: expirationDate,
+            voucher: 0,
+            isValid: false,
+          },
+        });
       }
 
-      // add new user voucher in voucherpoint
-      const newVoucherPoint = await prisma.voucherPoint.create({
+      data.reference_code = reference_code;
+      console.log("point added for referrer");
+    }
+    // add new user using referrer's ref code
+    newUser = await prisma.user.create({
+      data,
+    });
+
+    // add new user voucher in voucherpoint
+    if (reference_code)
+      await prisma.voucherPoint.create({
         data: {
           user_id: newUser.id,
           point: 0,
@@ -163,47 +170,51 @@ class UserService {
           isValid: true,
         },
       });
-      console.log("point added to new user", newVoucherPoint);
-    } else {
-      // if new user doesnt use any ref code
-      newUser = await prisma.user.create({
-        data,
-      });
-      console.log(newUser, "user no ref");
-    }
-    const verifyToken = createToken({ id: newUser.id }, "15m");
+
+    const verifyToken = createToken({ id: newUser.id }, "5m");
+
+    const template = fs
+      .readFileSync(__dirname + "/../templates/verification.html")
+      .toString();
+
+    const html = render(template, {
+      email: data.email,
+      first_name: data.first_name,
+      verify_url: `http://localhost:3000/users/verification/${verifyToken}`,
+    });
+
+    console.log("rendering template");
 
     transporter.sendMail({
       to: data.email,
       subject: "Welcome To Tickzy, Please Verify Your Email Address",
-      html: `<h1 classname= text-blue-700>Thank you for registering with Tickzy! We're excited to have you on board.</h1> <a href="http://localhost:3000/users/verification/${verifyToken}">verify your email here</a>`,
+      html,
     });
   }
 
   async sendVerification(req: Request) {
     try {
-    const { token } = req.params;
-    const user = verify(token, SECRET_KEY) as TUser;
+      const { token } = req.params;
+      const user = verify(token, SECRET_KEY) as TUser;
 
-    if (!user || !user.id) {
-      throw new Error("Invalid token/user");
+      if (!user || !user.id) {
+        throw new Error("Invalid token/user");
+      }
+
+      await prisma.user.update({
+        where: {
+          id: user?.id,
+        },
+        data: {
+          isVerified: true,
+        },
+      });
+
+      return { success: true };
+    } catch (error) {
+      console.log("Error sending verification");
     }
-    
-    await prisma.user.update({
-      where: {
-        id: user?.id,
-      },
-      data: {
-        isVerified: true,
-      },
-    });
-
-    return { success: true};
-  } catch (error) {
-    console.log("Error sending verification");
-    
   }
 }
-};
 
 export default new UserService();
