02308e482bc6ec1db9c0606d4711a3aa3037b008
astnab <astnab@gmail.com>
2024-05-30 14:20:45 +0700

    fixed login page

--- client/src/app/users/v2/page.tsx
+++ client/src/app/users/v2/page.tsx
@@ -1,14 +1,9 @@
 "use client";
-import React from 'react';
-import LoginForm from './_component/LoginForm';
-import { store } from '@/lib/redux/store';
+import React from "react";
+import LoginForm from "./_component/LoginForm";
+
 import { Provider } from "react-redux";
 
-
 export default function LoginPage() {
-  return (
-    <Provider store={store}>
-    <LoginForm/>
-    </Provider>
-  )
+  return <LoginForm />;
 }
