f52c6a4b57742e3e4701f9ab5c91249e7a5e29b0
astnab <astnab@gmail.com>
2024-05-31 13:56:00 +0700

    deleted/moved folders in client

--- client/src/app/users/v2/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import React from "react";
-import LoginForm from "./_component/LoginForm";
-
-import { Provider } from "react-redux";
-
-export default function LoginPage() {
-  return <LoginForm />;
-}
