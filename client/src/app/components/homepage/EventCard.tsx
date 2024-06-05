985607b9a8d073d12845ffe5016f6c9ad842dbe8
astnab <astnab@gmail.com>
2024-06-05 12:19:16 +0700

    fixed card

--- client/src/app/components/homepage/EventCard.tsx
+++ client/src/app/components/homepage/EventCard.tsx
@@ -1,37 +1,105 @@
-import React from "react";
+"use client";
+import { axiosInstance } from "@/lib/axios.config";
+import { useEffect, useState } from "react";
+import dayjs from "dayjs";
+import { useRouter } from "next/navigation";
 
-function EventCard() {
+interface Event {
+  id: string;
+  banner: string;
+  title: string;
+  description: string;
+  start_time: string;
+  location: string;
+  ticket_price: number;
+  promotor: string;
+  discount_price?: number;
+  promo?: string;
+}
+
+const EventCard: React.FC = () => {
+  const router = useRouter();
+
+  const [events, setEvents] = useState<Event[]>([]);
+
+  const fetchEventData = async () => {
+    try {
+      const response = await axiosInstance().get("/event/");
+      const { data } = response.data;
+      const formattedData = data.map((event: any) => ({
+        ...event,
+        banner: `data:image/jpeg;base64,${Buffer.from(
+          event.banner,
+          "binary"
+        ).toString("base64")}`,
+      }));
+      setEvents(formattedData);
+    } catch (error) {
+      console.error("Error fetching event data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchEventData();
+  }, []);
+
+  const formatPrice = (price: number) => {
+    return price.toLocaleString("en-ID");
+  };
+
+  const redirectToEvent = (eventId: string) => {
+    router.push(`/event/${eventId}`);
+  };
   return (
-    <div className="w-full">
-      <div className="flex flex-row flex-wrap">
-        <div className="border w-full flex flex-col rounded-lg shadow-md overflow-hidden truncate cursor-pointer">
-          <div className=" ">
-            <img
-              src="https://s3-ap-southeast-1.amazonaws.com/loket-production-sg/images/banner/20240528022521.jpg"
-              alt=""
-              className="object-cover h-32"
-            />
-          </div>
-          <div className="p-3 gap-2 flex flex-col">
-            <div className="text-md ">Musikal Keluarga Cemara</div>
-            <div className="text-md">17 January 2024</div>
-            <div className="font-bold">IDR 120.000</div>
-            <div className="border my-2" />
-
-            <div className="flex flex-row  gap-2">
-              <div className="rounded-full h-6 w-6">
+    <div className="">
+      <div className="flex flex-row flex-wrap gap-6 justify-center">
+        {events.length > 0 &&
+          events.map((event) => (
+            <div
+              key={event.id}
+              className="border w-80 flex flex-col rounded-lg shadow-md overflow-hidden truncate cursor-pointer"
+              onClick={() => redirectToEvent(event.id)}
+            >
+              <div className="h-32 relative">
                 <img
-                  src="https://s3-ap-southeast-1.amazonaws.com/loket-production-sg/images/organization/20240528101652.png"
+                  src={event.banner}
                   alt=""
+                  className="object-cover w-full h-full"
+                  // onLoad={handleImageLoad} // Cleanup temporary URL on load
                 />
               </div>
-              <div>PT Visinema</div>
+              <div className="p-3 gap-2 flex flex-col">
+                <div className="text-md font-bold">{event.title}</div>
+                <div className="text-md">
+                  {dayjs(event.start_time).format("DD MMMM YYYY")}
+                </div>{" "}
+                <div className="font-bold">
+                  {event.promo && event.discount_price !== undefined
+                    ? `IDR ${formatPrice(event.discount_price)}`
+                    : event.ticket_price === 0
+                    ? "FREE"
+                    : `IDR ${formatPrice(event.ticket_price)}`}
+                </div>
+                {/* <div className="font-bold">
+                  {event.promo && event.discount_price !== undefined ? (
+                    <div>
+                      <span className="line-through mr-2">IDR {formatPrice(event.ticket_price)}</span>
+                      <span>IDR {formatPrice(event.discount_price)}</span>
+                    </div>
+                  ) : (
+                    event.ticket_price === 0 ? "FREE" : `IDR ${formatPrice(event.ticket_price)}`
+                  )}
+                </div> */}
+                <div className="border my-2" />
+                <div className="flex flex-row  gap-2">
+                  <div>{event.promotor}</div>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+          ))}
       </div>
     </div>
   );
-}
+};
 
 export default EventCard;
