e58f9d7567ade9c24e3e55e78b3a92b321b9ecff
astnab <astnab@gmail.com>
2024-06-06 13:39:32 +0700

    edited env and axios config (vps api)

--- client/src/lib/axios.config.ts
+++ client/src/lib/axios.config.ts
@@ -1,16 +1,14 @@
 import axios from "axios";
 import { getCookie } from "cookies-next";
-
+const baseURL = process.env.NEXT_PUBLIC_API_BASEURL || "http://localhost:8000";
 export function axiosInstance() {
-
   const token = getCookie("auth") || "";
 
   return axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL,
     headers: {
       Authorization: "Bearer" + token,
     },
     withCredentials: true,
   });
-  
 }
