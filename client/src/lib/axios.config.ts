b6f48aa6b6465d464b1b0a13c2254419b9d5d3a9
ccandradewi <cornelia.candradewi@outlook.com>
2024-06-06 14:21:35 +0700

    fix axios token

--- client/src/lib/axios.config.ts
+++ client/src/lib/axios.config.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getCookie } from "cookies-next";
 const baseURL = process.env.NEXT_PUBLIC_API_BASEURL || "http://localhost:8000";
 export function axiosInstance() {
-  const token = getCookie("auth") || "";
+  const token = getCookie("access_token") || "";
 
   return axios.create({
     baseURL,
