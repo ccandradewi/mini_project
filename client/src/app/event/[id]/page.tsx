2380bd73aea198ef4e489c7b87166760aa06a36a
astnab <astnab@gmail.com>
2024-06-10 11:13:27 +0700

    fixed import navbar

--- client/src/app/event/[id]/page.tsx
+++ client/src/app/event/[id]/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "@/app/components/Navbar";
+import Navbar from "@/app/components/navbar";
 import EventDetails from "./_components/EventDetails";
 
 function page() {
