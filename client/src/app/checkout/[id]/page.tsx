36d06248e8794e6ffd3e18955ba0a7552067a0b5
astnab <astnab@gmail.com>
2024-06-11 16:32:00 +0700

    edit style in fe

--- client/src/app/checkout/[id]/page.tsx
+++ client/src/app/checkout/[id]/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import DetailCheckouts from "./_components/DetailCheckouts";
 import Navbar from "@/app/components/navbar";
+import Footer from "@/app/components/Footer";
 
 function page() {
   return (
     <>
       <Navbar />
       <DetailCheckouts />
+      <Footer />
     </>
   );
 }
