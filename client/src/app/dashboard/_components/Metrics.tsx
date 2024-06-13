81277b28a92c71cb95c1acdafce765ca3ebb8181
astnab <astnab@gmail.com>
2024-06-13 15:36:49 +0700

    added loading

--- client/src/app/dashboard/_components/Metrics.tsx
+++ client/src/app/dashboard/_components/Metrics.tsx
@@ -1,6 +1,5 @@
 "use client";
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { axiosInstance } from "@/lib/axios.config";
 import { AiFillSchedule } from "react-icons/ai";
@@ -64,13 +63,14 @@ function Metrics() {
   const [loading, setLoading] = useState(true);
 
   const fetchMetrics = async () => {
+    setLoading(true);
     try {
       const response = await axiosInstance().get("/dashboard/metric");
       const { data } = response.data;
       setMetricData(data);
-      setLoading(false);
     } catch (error) {
       console.error("Error fetching metrics data:", error);
+    } finally {
       setLoading(false);
     }
   };
@@ -78,6 +78,14 @@ function Metrics() {
   useEffect(() => {
     fetchMetrics();
   }, []);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-full">
+        <span className="loading loading-dots loading-lg"></span>
+      </div>
+    );
+  }
 
   if (!metricData) {
     return <div>No data available</div>;
@@ -105,36 +113,32 @@ function Metrics() {
   ];
 
   return (
-    <>
-      <div>
-        <div className="">
-          <div className="flex flex-row justify-center items-center gap-8">
-            {/* CHART */}
-            <div className="border rounded-lg h-full w-2/3 px-2">
-              <EventChart data={metricData.ticketsAndRevenuePerEvent} />
-            </div>
+    <div>
+      <div className="flex flex-row justify-center items-center gap-8">
+        {/* CHART */}
+        <div className="border rounded-lg h-full w-2/3 px-2">
+          <EventChart data={metricData.ticketsAndRevenuePerEvent} />
+        </div>
 
-            {/* ANGKA TOTAL */}
-            <div className="flex flex-col w-1/3 h-full gap-8">
-              {metrics.map((metric) => (
-                <div
-                  key={metric.id}
-                  className="flex flex-col border rounded-lg px-4 items-start py-3 w-60"
-                >
-                  <div className="mb-[30px] text-lg rounded-full bg-zinc-300 p-2 text-[#4f4e75]">
-                    {metric.icon}
-                  </div>
-                  <div className="text-xs">{metric.label}</div>
-                  <div className="font-bold text-2xl text-[#4f4e75]">
-                    {metric.value}
-                  </div>
-                </div>
-              ))}
+        {/* ANGKA TOTAL */}
+        <div className="flex flex-col w-1/3 h-full gap-8">
+          {metrics.map((metric) => (
+            <div
+              key={metric.id}
+              className="flex flex-col border rounded-lg px-4 items-start py-3 w-60"
+            >
+              <div className="mb-[30px] text-lg rounded-full bg-zinc-300 p-2 text-[#4f4e75]">
+                {metric.icon}
+              </div>
+              <div className="text-xs">{metric.label}</div>
+              <div className="font-bold text-2xl text-[#4f4e75]">
+                {metric.value}
+              </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
